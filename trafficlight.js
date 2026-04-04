9fff32359bd9e35831b81a9b38c1d66613b82d82
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-04-04 21:03:42 +0530

    trafficlight

---
+++ trafficlight.js
@@ -0,0 +1,12 @@
+let color ="green";
+
+//Traffic Light System
+if(color === "red"){
+console.log("stop! light is red ");
+}
+if(color === "yellow"){
+    console.log("slow down. color is yelow");
+}
+if(color === "green"){
+    console.log("go. light is green ");
+}
