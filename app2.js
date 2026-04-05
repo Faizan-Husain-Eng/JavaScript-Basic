26a5df27d881db713c4c90174bd6442f0f65480f
FAizan-Husain-Eng <faizanhusain2007@gmail.com>
2026-04-05 22:29:09 +0530

    index2

---
+++ app2.js
@@ -0,0 +1,11 @@
+//Conditional Staements
+console.log("before my if statemnet");
+let age = 23;
+if (age >= 18 ){
+    console.log("you can vote");
+}
+
+console.log("after my if statement");
+if (age > 20){
+    console.log("you are in your 20s");
+}
